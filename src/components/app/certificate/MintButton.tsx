45ee4175bcbb9030039a5cf712f5858ed9df224a
tidvn <tiendung0325@gmail.com>
2024-01-07 01:28:04 +0700

    fix

--- src/components/app/certificate/MintButton.tsx
+++ src/components/app/certificate/MintButton.tsx
@@ -17,21 +17,21 @@ import {
     MerkleTreeProof,
     MerkleTree,
   } from "@solana/spl-account-compression";
-  import {
-    PROGRAM_ID as BUBBLEGUM_PROGRAM_ID,
-    MetadataArgs,
-    TokenProgramVersion,
-    TokenStandard,
-    createCreateTreeInstruction,
-    createMintToCollectionV1Instruction,
-  } from "@metaplex-foundation/mpl-bubblegum";
-  import {
-    PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID,
-    CreateMetadataAccountArgsV3,
-    createCreateMetadataAccountV3Instruction,
-    createCreateMasterEditionV3Instruction,
-    createSetCollectionSizeInstruction,
-  } from "@metaplex-foundation/mpl-token-metadata";
+//   import {
+//     PROGRAM_ID as BUBBLEGUM_PROGRAM_ID,
+//     MetadataArgs,
+//     TokenProgramVersion,
+//     TokenStandard,
+//     createCreateTreeInstruction,
+//     createMintToCollectionV1Instruction,
+//   } from "@metaplex-foundation/mpl-bubblegum";
+//   import {
+//     PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID,
+//     CreateMetadataAccountArgsV3,
+//     createCreateMetadataAccountV3Instruction,
+//     createCreateMasterEditionV3Instruction,
+//     createSetCollectionSizeInstruction,
+//   } from "@metaplex-foundation/mpl-token-metadata";
   import { Metadata, MetaplexError, Option } from "@metaplex-foundation/js";
 
 import { useWallet } from "@solana/wallet-adapter-react"
@@ -52,49 +52,49 @@ export const MintButton = (props: any) => {
         const mintIxs: TransactionInstruction[] = [];
 
 
-        certMemberList.map(async (item: any) => {
-            mintIxs.push(
-                MintToCollectionV1InstructionData(
-                    {
-                        payer: payer.publicKey,
+        // certMemberList.map(async (item: any) => {
+        //     mintIxs.push(
+        //         createMintToCollectionV1Instruction(
+        //             {
+        //                 payer: PublicKey,
 
-                        merkleTree: treeAddress,
-                        treeAuthority,
-                        treeDelegate: payer.publicKey,
+        //                 merkleTree: treeAddress,
+        //                 treeAuthority,
+        //                 treeDelegate: payer.publicKey,
 
-                        // set the receiver of the NFT
-                        leafOwner: receiverAddress || payer.publicKey,
-                        // set a delegated authority over this NFT
-                        leafDelegate: payer.publicKey,
+        //                 // set the receiver of the NFT
+        //                 leafOwner: receiverAddress || payer.publicKey,
+        //                 // set a delegated authority over this NFT
+        //                 leafDelegate: payer.publicKey,
 
-                        /*
-                            You can set any delegate address at mint, otherwise should 
-                            normally be the same as `leafOwner`
-                            NOTE: the delegate will be auto cleared upon NFT transfer
-                            ---
-                            in this case, we are setting the payer as the delegate
-                          */
+        //                 /*
+        //                     You can set any delegate address at mint, otherwise should 
+        //                     normally be the same as `leafOwner`
+        //                     NOTE: the delegate will be auto cleared upon NFT transfer
+        //                     ---
+        //                     in this case, we are setting the payer as the delegate
+        //                   */
 
-                        // collection details
-                        collectionAuthority: payer.publicKey,
-                        collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
-                        collectionMint: collectionMint,
-                        collectionMetadata: collectionMetadata,
-                        editionAccount: collectionMasterEditionAccount,
+        //                 // collection details
+        //                 collectionAuthority: payer.publicKey,
+        //                 collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
+        //                 collectionMint: collectionMint,
+        //                 collectionMetadata: collectionMetadata,
+        //                 editionAccount: collectionMasterEditionAccount,
 
-                        // other accounts
-                        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-                        logWrapper: SPL_NOOP_PROGRAM_ID,
-                        bubblegumSigner: bubblegumSigner,
-                        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-                    },
-                    {
-                        metadataArgs,
-                    },
-                ),
-            );
+        //                 // other accounts
+        //                 compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+        //                 logWrapper: SPL_NOOP_PROGRAM_ID,
+        //                 bubblegumSigner: bubblegumSigner,
+        //                 tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+        //             },
+        //             {
+        //                 metadataArgs,
+        //             },
+        //         ),
+        //     );
 
-        })
+        // })
     }
     return (<Button onClick={handlerMint}>Mint On Solana</Button>)
 }
